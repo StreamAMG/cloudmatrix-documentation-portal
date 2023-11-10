9738b3188b189710f64552c2fd2885f4e7e1d7bd
jonathon.robins@pamediagroup.com <jonathon.robins@pamediagroup.com>
2023-11-10 07:49:33 +0000

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjc2ODM3/spectral.js?branch_slug=main&token=5265a871-af7c-49c0-8fe0-330630163138";
+export default { extends: ruleset };
